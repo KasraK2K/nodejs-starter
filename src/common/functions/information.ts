9c88b24e2dd9f3856ca0c2d549491911278fa259
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-02-26 00:50:57 +0330

    make index file cleaner

---
+++ src/common/functions/information.ts
@@ -0,0 +1,49 @@
+//=======================================================================================================================================================
+//
+//   ####  #####  #####    ##   ##  #####  #####          ##  ##     ##  #####   #####   #####    ###    ###    ###    ######  ##   #####   ##     ##
+//  ##     ##     ##  ##   ##   ##  ##     ##  ##         ##  ####   ##  ##     ##   ##  ##  ##   ## #  # ##   ## ##     ##    ##  ##   ##  ####   ##
+//   ###   #####  #####    ##   ##  #####  #####          ##  ##  ## ##  #####  ##   ##  #####    ##  ##  ##  ##   ##    ##    ##  ##   ##  ##  ## ##
+//     ##  ##     ##  ##    ## ##   ##     ##  ##         ##  ##    ###  ##     ##   ##  ##  ##   ##      ##  #######    ##    ##  ##   ##  ##    ###
+//  ####   #####  ##   ##    ###    #####  ##   ##        ##  ##     ##  ##      #####   ##   ##  ##      ##  ##   ##    ##    ##   #####   ##     ##
+//
+//=======================================================================================================================================================
+
+import os from "os";
+
+export const getUserInformation = (port: Number) => {
+  console.group("Server Information:");
+  console.table([
+    {
+      Port: port,
+      NODE_ENV: process.env.NODE_ENV,
+      Platform: os.platform(),
+      "CPU Model": os.cpus()[0].model,
+      Arch: os.arch(),
+    },
+  ]);
+  console.groupEnd();
+  // CPU/Ram Information
+  console.group("\nCPU/Ram Information:");
+  console.table([
+    {
+      "CPU Count": os.cpus().length,
+      "CPU Speed": os.cpus()[0].speed,
+      "Total Memory": os.totalmem(),
+      "Free Memory": os.freemem(),
+      "Used Memory": os.totalmem() - os.freemem(),
+    },
+  ]);
+  console.groupEnd();
+  // Node Information
+  console.group("\nNode Information:");
+  console.table([
+    {
+      "Node PID": process.pid,
+      "Node CPU Usage": process.cpuUsage(),
+      "Node Version": process.version,
+      "Node Exec Path": process.execPath,
+    },
+  ]);
+  console.groupEnd();
+  console.info(`\nServer running on http://localhost:${port}`);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import "dotenv/config";
-import os from "os";
 import express from "express";
 import { Express, Request, Response } from "express";
 import _ from "lodash";
 const { locals, globals } = require("./common/variabels");
 import router from "./gateway/router";
 import tokenInterceptor from "./gateway/interceptor/TokenInterceptor";
+import dotenv from "dotenv";
+import { getUserInformation } from "./common/functions/information";
 
 class Application {
   public app: Express;
@@ -16,9 +16,20 @@ class Application {
     this.app = express();
     this.port = Number(process.env.PORT) || port;
 
+    this.config();
     this.middlewares();
     this.routes();
-    this.config();
+  }
+
+  private config() {
+    dotenv.config({
+      path:
+        process.env.NODE_ENV === "production"
+          ? ".env.production"
+          : ".env.development",
+    });
+    this.app.locals = locals;
+    _.assign(global, globals);
   }
 
   private middlewares() {
@@ -27,57 +38,13 @@ class Application {
     this.app.disable("x-powered-by");
   }
 
-  private config() {
-    this.app.locals = locals;
-    _.assign(global, globals);
-  }
-
   private routes() {
     this.app.use("*", tokenInterceptor.verify);
     this.app.use("/", router);
   }
 
-  private information() {
-    // Server Information
-    console.group("Server Information:");
-    console.table([
-      {
-        Port: this.port,
-        NODE_ENV: process.env.NODE_ENV,
-        Platform: os.platform(),
-        "CPU Model": os.cpus()[0].model,
-        Arch: os.arch(),
-      },
-    ]);
-    console.groupEnd();
-    // CPU/Ram Information
-    console.group("\nCPU/Ram Information:");
-    console.table([
-      {
-        "CPU Count": os.cpus().length,
-        "CPU Speed": os.cpus()[0].speed,
-        "Total Memory": os.totalmem(),
-        "Free Memory": os.freemem(),
-        "Used Memory": os.totalmem() - os.freemem(),
-      },
-    ]);
-    console.groupEnd();
-    // Node Information
-    console.group("\nNode Information:");
-    console.table([
-      {
-        "Node PID": process.pid,
-        "Node CPU Usage": process.cpuUsage(),
-        "Node Version": process.version,
-        "Node Exec Path": process.execPath,
-      },
-    ]);
-    console.groupEnd();
-    console.info(`\nServer running on http://localhost:${this.port}`);
-  }
-
   public init() {
-    this.app.listen(this.port, () => this.information());
+    this.app.listen(this.port, () => getUserInformation(this.port));
   }
 }
 
