8e7db2398dc2061d295dcb238324862e5d27cde0
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-02-26 18:15:20 +0330

    change mongoose connection to be multiple

--- src/boot/database.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose
-  .connect(process.env.MONGODB_URI!)
-  .then(() => {
-    console.log("Database connected");
-  })
-  .catch((error) => console.error(error.message));
