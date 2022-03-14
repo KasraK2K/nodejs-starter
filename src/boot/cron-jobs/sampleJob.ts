c641b71a51d43b169c260f021a2d7fbef76509d5
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-03-14 17:12:17 +0330

    add cronjobs and write some documents

--- src/boot/cron-jobs/sampleJob.ts
+++ src/boot/cron-jobs/sampleJob.ts
@@ -4,14 +4,14 @@ let cycle = 1;
 const maxCycles = 3;
 
 const sampleJob = new CronJob(
-  "* * * * * *",
+  "* * * 10-20 * *",
   function () {
     console.log(`LOG SAMPLE JOB CYCLE ${cycle}/${maxCycles}`);
     if (cycle++ === maxCycles) sampleJob.stop();
   },
   null,
   true,
-  "America/Los_Angeles"
+  "Asia/Tehran"
 );
 sampleJob.start();
 
