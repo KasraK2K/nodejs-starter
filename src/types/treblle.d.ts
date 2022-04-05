bf66187e42a02542deb8b9ba70f97f75f8599585
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-04-05 23:24:17 +0430

    add treblle type

--- src/types/treblle.d.ts
+++ src/types/treblle.d.ts
@@ -7,3 +7,37 @@
 //    ##       ##     ##      #####        ####    #####   ####  ######  #####  ##   ##  ##   ##    ##    ##   #####   ##     ##
 //
 //==================================================================================================================================
+
+declare module "treblle" {
+  type OptionsBase = {
+    apiKey: string;
+    projectId: string;
+    additionalFieldsToMask?: string[];
+    showErrors?: boolean;
+  };
+
+  type FetchEvent = any;
+  type Request = any;
+  type Response = any;
+
+  export function useTreblle(app: any, options: any): void;
+
+  export function koaTreblle(
+    apiKey: string,
+    projectId: string,
+    additionalFieldsToMask?: any[],
+    showErrors?: boolean
+  ): Function;
+
+  export function strapiTreblle(
+    apiKey: string,
+    projectId: string,
+    additionalFieldsToMask?: any[],
+    showErrors?: boolean,
+    ignoreAdminRoutes?: string[]
+  ): Function;
+
+  export function serviceWorkerTreblle(options: OptionsBase): (event: FetchEvent) => void;
+
+  export function moduleWorkerTreblle(options: OptionsBase): (request: Request) => Promise<Response> | Response;
+}
