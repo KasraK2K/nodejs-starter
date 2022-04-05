94dab4d1f02d6cd12721240de05f63653cebd492
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-04-05 23:28:43 +0430

    change type declerations

--- src/common/variabels.ts
+++ src/common/variabels.ts
@@ -56,27 +56,6 @@ export const globals = {
   logger,
 };
 
-// ──────────────────────────────────────────────────────────────────────────────────────────────────
-//   :::::: D E C L E A R   G L O B A L   V A R I A B L E S : :  :   :    :     :        :          :
-// ──────────────────────────────────────────────────────────────────────────────────────────────────
-declare global {
-  const service: typeof globals.service;
-  const validator: typeof globals.validator;
-  const schema: typeof globals.schema;
-  const objectValidator: typeof globals.objectValidator;
-  const objectSchema: typeof globals.objectSchema;
-  const mongo: {
-    mongoClient: typeof globals.mongo.mongoClient;
-    database: typeof globals.mongo.database;
-    collection: typeof globals.mongo.collection;
-  };
-  const pg: {
-    pool: typeof globals.pg.pool;
-  };
-  const logger: typeof globals.logger;
-  const process_id: string;
-}
-
 export default {
   locals,
   globals,
