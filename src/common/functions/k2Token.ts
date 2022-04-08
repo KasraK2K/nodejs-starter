ae0d1f1b0c313b5efc64391824c3ed3e4b0401dd
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-04-08 11:07:18 +0430

    change k2Token to instance of K2Token class

--- src/common/functions/k2Token.ts
+++ src/common/functions/k2Token.ts
@@ -1,51 +1,55 @@
 import "../../boot/envirement";
 
-const secretKey = process.env.SECURE_SECRET_KEY ?? "Jsa0lmxkVuTFhJpYXQiOjObFkrNmV4sInRSjFjMlYplzvCCwP3";
-const secretFirstKeySeparator =
-  process.env.SECURE_SECRET_FIRST_SEPARATOR ?? "NiMjVuTFhObFk0SmxkQzFyWlhrNmV5SjFjMlZ5WDJsa0lqb";
-const secretSecondKeySeparator =
-  process.env.SECURE_SECRET_SECOND_SEPARATOR ?? "tk6MZRVCI6IkpXVCObFk0SmxVuTFV5SjFjMlZ5WMDM5NjV9";
+class K2Token {
+  constructor(
+    private secretKey = process.env.SECURE_SECRET_KEY ?? "Jsa0lmxkVuTFhJpYXQiOjObFkrNmV4sInRSjFjMlYplzvCCwP3",
+    private secretFirstKeySeparator = process.env.SECURE_SECRET_FIRST_SEPARATOR ??
+      "NiMjVuTFhObFk0SmxkQzFyWlhrNmV5SjFjMlZ5WDJsa0lqb",
+    private secretSecondKeySeparator = process.env.SECURE_SECRET_SECOND_SEPARATOR ??
+      "tk6MZRVCI6IkpXVCObFk0SmxVuTFV5SjFjMlZ5WMDM5NjV9"
+  ) {}
 
-export const secure = (arg: any): string => {
-  const buffSecretKey = Buffer.from(secretKey, "utf-8");
-  const strSecretKey = buffSecretKey.toString("base64");
+  public generateToken(payload: any): string {
+    const buffSecretKey = Buffer.from(this.secretKey, "utf-8");
+    const strSecretKey = buffSecretKey.toString("base64");
 
-  const buffFirstKeySep = Buffer.from(secretFirstKeySeparator, "utf-8");
-  const strFirstKeySep = buffFirstKeySep.toString("base64");
+    const buffFirstKeySep = Buffer.from(this.secretFirstKeySeparator, "utf-8");
+    const strFirstKeySep = buffFirstKeySep.toString("base64");
 
-  const bufferArg = Buffer.from(JSON.stringify(arg), "utf-8");
-  const strArg = bufferArg.toString("base64");
+    const bufferArg = Buffer.from(JSON.stringify(payload), "utf-8");
+    const strArg = bufferArg.toString("base64");
 
-  const buffSecondKeySep = Buffer.from(secretSecondKeySeparator, "utf-8");
-  const strSecondKeySep = buffSecondKeySep.toString("base64");
+    const buffSecondKeySep = Buffer.from(this.secretSecondKeySeparator, "utf-8");
+    const strSecondKeySep = buffSecondKeySep.toString("base64");
 
-  return strSecretKey + strFirstKeySep + strArg + strSecondKeySep;
-};
-
-export const deSecure = (arg: string): any => {
-  const buff = Buffer.from(arg, "base64");
-  const str = buff.toString("utf-8");
-  const key = str.slice(0, str.indexOf(secretFirstKeySeparator));
-  const stringData = str.slice(
-    str.indexOf(secretFirstKeySeparator) + secretFirstKeySeparator.length,
-    str.indexOf(secretSecondKeySeparator)
-  );
-  if (key !== secretKey) throw new Error("Invalid key");
-  else return JSON.parse(stringData);
-};
-
-export const isValidSecure = (arg: string): boolean => {
-  try {
-    const buff = Buffer.from(arg, "base64");
-    const str = buff.toString("utf-8");
-    const key = str.slice(0, str.indexOf(secretFirstKeySeparator));
-    const strSecondSep = str.slice(str.indexOf(secretSecondKeySeparator));
-    if (key !== secretKey) return false;
-    else if (strSecondSep !== secretSecondKeySeparator) return false;
-    else return true;
-  } catch (error) {
-    return false;
+    return strSecretKey + strFirstKeySep + strArg + strSecondKeySep;
   }
-};
 
-export default { secure, deSecure, isValidSecure };
+  public verifyToken(token: string): any {
+    const buff = Buffer.from(token, "base64");
+    const str = buff.toString("utf-8");
+    const key = str.slice(0, str.indexOf(this.secretFirstKeySeparator));
+    const stringData = str.slice(
+      str.indexOf(this.secretFirstKeySeparator) + this.secretFirstKeySeparator.length,
+      str.indexOf(this.secretSecondKeySeparator)
+    );
+    if (key !== this.secretKey) throw new Error("Invalid key");
+    else return JSON.parse(stringData);
+  }
+
+  public isValidToken(token: string): boolean {
+    try {
+      const buff = Buffer.from(token, "base64");
+      const str = buff.toString("utf-8");
+      const key = str.slice(0, str.indexOf(this.secretFirstKeySeparator));
+      const strSecondSep = str.slice(str.indexOf(this.secretSecondKeySeparator));
+      if (key !== this.secretKey) return false;
+      else if (strSecondSep !== this.secretSecondKeySeparator) return false;
+      else return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
+
+export default new K2Token();
